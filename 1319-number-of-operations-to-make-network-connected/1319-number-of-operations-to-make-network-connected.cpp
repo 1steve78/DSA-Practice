6f52f857783d77fa1f1002db7d016b7d268b000f
MD YASIN ALAM <143877274+1steve78@users.noreply.github.com>
2026-07-15 21:04:03 +0530

    Time: 4 ms (79.86%), Space: 42.2 MB (83.42%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,35 +1,54 @@
-class Solution {
-public:
-    void dfs(int node, vector<int>& vis, vector<vector<int>>& graph) {
-        vis[node] = 1;
+class DisjointSet{
+public : 
+    vector<int> parent, size;
 
-        for (int adj : graph[node]) {
-            if (!vis[adj])
-                dfs(adj, vis, graph);
-        }
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n,1);
+        
+        for(int i = 0 ; i< n ; i++) parent[i]=i;
     }
 
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if (connections.size() < n - 1)
-            return -1;
+    int findPar(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=findPar(parent[x]);
+    }
 
-        vector<vector<int>> graph(n);
+    void unionBySize(int u , int v){
+        int pu = findPar(u);
+        int pv = findPar(v);
 
-        for (auto &it : connections) {
-            graph[it[0]].push_back(it[1]);
-            graph[it[1]].push_back(it[0]);
+        if(pv == pu) return ;
+
+        if(size[pu]<size[pv]){
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
         }
+    }
+};
 
-        vector<int> vis(n, 0);
+class Solution {
+public:
+  
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n-1) return -1;
+
+        DisjointSet ds(n);
+
         int components = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                dfs(i, vis, graph);
-                components++;
-            }
+        for(auto &it : connections){
+           ds.unionBySize(it[0],it[1]);
+
         }
 
+        for(int i = 0 ; i< n ;i++){
+            if(ds.findPar(i)==i)components++;
+        }
         return components - 1;
     }
 };

4ebb20d1b6d254301360c586e36d87bc708d7da2
MD YASIN ALAM <143877274+1steve78@users.noreply.github.com>
2026-08-24 09:11:44 +0530

    Time: 96 ms (86.38%), Space: 95.2 MB (49.63%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+       vector<int> dp(n);
+
+        vector<int> prefix(n);
+        prefix[0] = stones[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + stones[i];
+        }
+
+        dp[n-1]=prefix[n-1];
+        for (int i = n - 2; i >= 1; i--) {
+            dp[i] = max(prefix[i] - dp[i + 1], dp[i + 1]);
+        }
+
+        return dp[1];
+    }
+};

350ae3e264f6299e1b5fc706c3c74b00105f3172
MD YASIN ALAM <143877274+1steve78@users.noreply.github.com>
2026-09-08 09:50:21 +0530

    Time: 0 ms (100%), Space: 9.3 MB (5.52%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        string s = to_string(n);
+        int sz = s.size();
+        
+        if(sz <4) return 0;
+
+
+    
+        return n -999;
+    }
+};
